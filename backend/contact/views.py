9418de8f1d7545d3ac70d04038092cf2fdbabe1b
odajunpei <i.penju1022@gmail.com>
2023-01-07 17:06:57 +0900

    メール機能OK

--- backend/contact/views.py
+++ backend/contact/views.py
@@ -3,6 +3,7 @@ from .forms import ContactForm
 from django.http import HttpResponse
 from django.conf import settings
 from django.core.mail import BadHeaderError, send_mail
+from django.template.loader import render_to_string
 
 
 def contact_form(request):
@@ -10,15 +11,25 @@ def contact_form(request):
         form = ContactForm(request.POST)
         if form.is_valid():
             # 追記
+            username = form.cleaned_data['username']
             subject = form.cleaned_data['subject']
             message = form.cleaned_data['message']
+            tel = form.cleaned_data['tel']
             sender = form.cleaned_data['sender']
-            myself = form.cleaned_data['myself']
             recipients = [settings.EMAIL_HOST_USER]
-            if myself:
-                recipients.append(sender)
+            # recipients_user = sender
+            recipients.append(sender)
             try:
-                send_mail(subject, message, sender, recipients)
+                ctx = {
+                    "subject": subject,
+                    "username": username,
+                    "tel": tel,
+                    "message": message,
+                    "sender": sender
+                }
+                msg = render_to_string(
+                    f"{settings.BASE_DIR}/contact/templates/mails/user.txt", ctx, request)
+                send_mail(subject, msg, sender, recipients)
             except BadHeaderError:
                 return HttpResponse('無効なヘッダーが見つかりました。')
             return redirect('contact:complete')
